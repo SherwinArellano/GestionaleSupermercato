4a4a4459b3f5346e9781f7284d4fca5fef4b9d89
Sherwin Arellano <s.arellano@esis-italia.com>
2025-03-24 10:06:02 +0100

    Fix header prerender missing suspense

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,30 +1,51 @@
 import { auth } from '@/auth';
 import { AppSidebar } from '@/components/ui/dashboard/app-sidebar';
 import { SidebarProvider } from '@/components/ui/sidebar';
-import { ReactNode } from 'react';
+import { ReactNode, Suspense } from 'react';
 
 export default async function DashboardLayout({
   children,
 }: {
   children: ReactNode;
 }) {
-  const { user } = (await auth())!;
+  return (
+    <SidebarProvider>
+      <Suspense fallback={<AppSidebarSkeleton />}>
+        <UserAppSidebar />
+      </Suspense>
+
+      <div className="flex flex-1 flex-col">{children}</div>
+    </SidebarProvider>
+  );
+}
+
+function AppSidebarSkeleton() {
+  return (
+    <AppSidebar
+      user={{
+        name: `Loading...`,
+        avatar: '/',
+        avatarFallback: 'LO',
+        email: `loading@email`,
+      }}
+    />
+  );
+}
+
+async function UserAppSidebar() {
+  const user = (await auth())!.user;
   const initials =
     user.name.slice(0, 1).toUpperCase() +
     user.surname.slice(0, 1).toUpperCase();
 
   return (
-    <SidebarProvider>
-      <AppSidebar
-        user={{
-          name: `${user.name} ${user.surname}`,
-          avatar: '/',
-          avatarFallback: initials,
-          email: user.email,
-        }}
-      />
-
-      <div className="flex flex-1 flex-col">{children}</div>
-    </SidebarProvider>
+    <AppSidebar
+      user={{
+        name: `${user.name} ${user.surname}`,
+        avatar: '/',
+        avatarFallback: initials,
+        email: user.email,
+      }}
+    />
   );
 }
