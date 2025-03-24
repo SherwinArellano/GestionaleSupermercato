f4d651d8a1eae67e07ffc50422ac90f9073a3df9
Sherwin Arellano <s.arellano@esis-italia.com>
2025-03-24 09:57:12 +0100

    Enable experimental dynamicIO

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
     NEXT_PUBLIC_NODE_ENV: process.env.NODE_ENV,
     NEXT_PUBLIC_DB_BASE_URL: process.env.DB_BASE_URL,
   },
+  experimental: {
+    dynamicIO: true,
+  },
   async redirects() {
     return [
       {
