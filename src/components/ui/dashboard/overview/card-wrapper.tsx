14f78796217d434240b1d3e9a2846dc769650f7b
Sherwin Arellano <s.arellano@esis-italia.com>
2025-03-25 15:49:03 +0100

    Update overview suppliers count

--- src/components/ui/dashboard/overview/card-wrapper.tsx
+++ src/components/ui/dashboard/overview/card-wrapper.tsx
@@ -8,6 +8,17 @@ import { users } from '@/lib/db/users';
 import { unstable_cacheTag as cacheTag } from 'next/cache';
 
 export async function CardWrapper() {
+  return (
+    <>
+      <OverviewCard title="Total Sales" icon={Banknote} value={0} isCurrency />
+      <ProductsOverviewCard />
+      <SuppliersOverviewCard />
+      <OverviewCard title="Total Users" icon={Users} value={users.length} />
+    </>
+  );
+}
+
+async function ProductsOverviewCard() {
   'use cache';
 
   cacheTag('products');
@@ -32,15 +43,30 @@ export async function CardWrapper() {
         message="Could not fetch data. Server is probably down."
         isError={isError}
       />
-      <OverviewCard title="Total Sales" icon={Banknote} value={0} isCurrency />
       <OverviewCard
         title="Total Products"
         icon={Package}
         value={productsCount}
       />
-      <OverviewCard title="Total Suppliers" icon={Factory} value={0} />
-      <OverviewCard title="Total Users" icon={Users} value={users.length} />
     </>
+  );
+}
+
+async function SuppliersOverviewCard() {
+  'use cache';
+
+  cacheTag('suppliers');
+
+  // Simulate db request
+  await new Promise((resolve) => setTimeout(resolve, 200));
+  const { totalElements: suppliersCount } = await db.suppliers.get();
+
+  return (
+    <OverviewCard
+      title="Total Suppliers"
+      icon={Factory}
+      value={suppliersCount}
+    />
   );
 }
 
