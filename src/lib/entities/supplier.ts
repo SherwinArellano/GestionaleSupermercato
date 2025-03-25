310f51979fba7bb028130d7d2c84b05aaab62325
Sherwin Arellano <s.arellano@esis-italia.com>
2025-03-25 15:14:21 +0100

    Fix zod supplier schema

--- src/lib/entities/supplier.ts
+++ src/lib/entities/supplier.ts
@@ -2,11 +2,29 @@
 import { CreateSupplierDTO } from '@/types/db';
 import { z } from 'zod';
 
+// Phone Regex: https://stackoverflow.com/a/16699507/12294437
+const PHONE_NUMBER_REGEX =
+  /^(\+\d{1,2}\s?)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/;
+
 export const SupplierSchema = z.object({
   name: z.string().min(1, { message: 'Name is required.' }),
   address: z.string().min(1, { message: 'Address is required.' }),
-  email: z.string().min(1, { message: 'Address is required.' }),
-  phoneNumber: z.string().min(1, { message: 'Address is required.' }),
+  email: z
+    .string()
+    .email('Invalid email. E.g. mail@example.com')
+    .min(1, { message: 'Email is required.' }),
+  phoneNumber: z
+    .string()
+    .min(1, { message: 'Phone number is required.' })
+    .refine(
+      (value) => PHONE_NUMBER_REGEX.test(value),
+      (value) => ({
+        message:
+          value === ''
+            ? 'Phone number is required.'
+            : 'Invalid phone number. E.g. +39 321 456 7890',
+      })
+    ),
 } satisfies Record<keyof CreateSupplierDTO, any>);
 
 export type SupplierValues = z.infer<typeof SupplierSchema>;
