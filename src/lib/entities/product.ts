0cecdf0b72e86591c2aabfe667f02b8b97399e56
Sherwin Arellano <s.arellano@esis-italia.com>
2025-03-25 12:49:39 +0100

    Satisfy product interface on zod schema

--- src/lib/entities/product.ts
+++ src/lib/entities/product.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { CreateProductDTO } from '@/types/db';
 import { z } from 'zod';
 
 export const ProductSchema = z.object({
@@ -6,6 +8,6 @@ export const ProductSchema = z.object({
     .number()
     .gt(0, { message: 'Please enter an amount greater than €0.' }),
   category: z.string().min(1, { message: 'Category is required.' }),
-});
+} satisfies Record<keyof CreateProductDTO, any>);
 
 export type ProductValues = z.infer<typeof ProductSchema>;
