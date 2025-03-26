cf6b18e37a3abb9297e0ba52757fa173c5a2ed5d
Sherwin Arellano <s.arellano@esis-italia.com>
2025-03-26 09:25:06 +0100

    Fix user zod schema

--- src/lib/entities/user.ts
+++ src/lib/entities/user.ts
@@ -1,8 +1,11 @@
 import { z } from 'zod';
 
 export const UserSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(6),
+  email: z.string().email('Invalid email').min(1, 'Email is required'),
+  password: z
+    .string()
+    .min(6, 'Invalid password')
+    .min(1, 'Password is required'),
 });
 
 export type UserValues = z.infer<typeof UserSchema>;
